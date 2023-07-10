3d103f9b2d89adbe2051efd07648cacd6cc47ae4
wykstc <50796801+wykstc@users.noreply.github.com>
2023-07-10 22:01:51 +0900

    Update fusion_methods.py

--- EMP/model/fusion_methods.py
+++ EMP/model/fusion_methods.py
@@ -40,33 +40,15 @@ class AutoFusion(pl.LightningModule):
         BT = torch.softmax(torch.mul((torch.unsqueeze(B, dim=1)), T), dim=1)
         BV = torch.softmax(torch.mul((torch.unsqueeze(B, dim=1)), V), dim=1)
         BF = torch.softmax(torch.mul((torch.unsqueeze(B, dim=1)), F), dim=1)
-        BB = torch.softmax(torch.mul((torch.unsqueeze(B, dim=1)), B), dim=1)
 
         bba = torch.mm(BA,torch.unsqueeze(A, dim=1)).squeeze(1)
         bbt = torch.mm(BT,torch.unsqueeze(T, dim=1)).squeeze(1)
         bbv = torch.mm(BV,torch.unsqueeze(V, dim=1)).squeeze(1)
         bbf = torch.mm(BF,torch.unsqueeze(F, dim=1)).squeeze(1)
-        bbb = torch.mm(BB,torch.unsqueeze(B, dim=1)).squeeze(1)
-
-        #vaft
-        #compressed_z = self.fuse_inGlobal(torch.cat((bba, bbt, bbv, bbf)))
-        #loss = self.criterion(self.fuse_outGlobal(compressed_z), torch.cat((bba, bbt, bbv, bbf)))
 
         compressed_z = self.fuse_inGlobal(torch.cat((bbt,bbv,bbf,bba)))
         loss = self.criterion(self.fuse_outGlobal(compressed_z), torch.cat((bbt,bbv,bbf,bba)))
 
-        #vfta
-        #compressed_z = self.fuse_inGlobal(torch.cat((bbv,bbf,bbt)))
-        #loss = self.criterion(self.fuse_outGlobal(compressed_z), torch.cat((bbv,bbf,bbt)))
-
-        #compressed_z = self.fuse_inGlobal(torch.cat((A,T,V,F)))
-        #loss = self.criterion(self.fuse_outGlobal(compressed_z), torch.cat((A,T,V,F)))
-
-        #compressed_z = self.fuse_inGlobal(bba)
-        #loss = self.criterion(self.fuse_outGlobal(compressed_z), bba)
-
-        #return compressed_z.unsqueeze(0), 0
-        #return torch.cat((bba,bbt,bbv,bbf)).unsqueeze(0), 0
         return compressed_z.unsqueeze(0), loss
 
 
